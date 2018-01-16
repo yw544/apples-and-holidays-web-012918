4065048afab8f134a57f9d17da5e36b37538c77d
York Wang <yw544@nyu.edu>
2018-01-16 15:01:29 +0000

    automatically backed up by learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -69,7 +69,7 @@ def all_supplies_in_holidays(holiday_hash)
   #   Fourth Of July: Fireworks, BBQ
   # etc.
 holiday_hash.each do |season, value|
-  season = seasn.capitalize
+  season = season.capitalize
   puts "#{season}:"
   value.each do |holiday, value|
     holiday_name_array = holiday.to_s.split("_")
