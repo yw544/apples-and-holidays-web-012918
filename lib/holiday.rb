3f65cc0805a32089a257ed0b6195f1b2834e351c
York Wang <yw544@nyu.edu>
2018-01-16 14:44:53 +0000

    automatically backed up by learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -26,7 +26,7 @@ def add_supply_to_winter_holidays(holiday_hash, supply)
   # holiday_hash is identical to the one above
   # add the second argument, which is a supply, to BOTH the
   # Christmas AND the New Year's arrays
-  holiday_hash[:spring][:memorial_day] << supply
+
 
 end
 
@@ -34,6 +34,7 @@ end
 def add_supply_to_memorial_day(holiday_hash, supply)
   # again, holiday_hash is the same as the ones above
   # add the second argument to the memorial day array
+    holiday_hash[:spring][:memorial_day] << supply
 
 end
 
